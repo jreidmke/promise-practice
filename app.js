a57ecf926273d11e308335e6444784e1e608c0f9
unknown <jreidmke@gmail.com>
2020-10-29 10:54:59 -0500

    added poke funcs

--- app.js
+++ app.js
@@ -57,3 +57,43 @@ function drawCard() {
 
 $(document).ready(shuffleDeck);
 $("#card-btn").on("click", drawCard);
+
+//so we want to get ALL (151) pokemon names and urls. this is just a very simple for in loop thing
+
+let pokemon;
+
+function getPokemon() {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+    .then(data => {
+        pokemon = data.data.results;
+    })
+    .catch(err => console.log(err));
+}
+
+function threeNums() {
+    let counter = 1;
+    let nums = [];
+    while(counter < 4) {
+        nums.push(Math.floor(Math.random() * 151));
+        counter++;
+    }
+    return nums;
+}
+
+function pickThree() {
+    const arr = threeNums();
+    for(num of arr) {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+        .then(data => {
+            $("#poke-div").append(`<div>
+            <h4>${data.data.name}</h4>
+            <img src=${data.data.sprites.front_default} alt="pokemon">
+        </div>`)
+            console.log(data);
+        })
+        .catch(err => console.log(err));
+    }
+}
+
+$(document).ready(getPokemon);
+$('#poke-btn').on("click", pickThree);
