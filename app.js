397b6057624ceef2f9cb0373dd28132b2091c818
unknown <jreidmke@gmail.com>
2020-10-29 10:21:19 -0500

    init one num four fax func

--- app.js
+++ app.js
@@ -7,4 +7,32 @@ function getOneNumFacts() {
     .catch(err => console.log(err))
 }
 
-$("#num-btn").on('click', getOneNumFacts)
+function getMoreFacts() {
+    const arr = [1, 2, 3, 4];
+    axios.get(`http://numbersapi.com/${arr}?json`)
+    .then(data => {
+        const vals = Object.values(data.data);
+        for(val of vals) {
+            $("#num-facts").append(`<li>${val}</li>`)
+        }
+    })
+    .catch(err => console.log(err));
+}
+
+function oneNumFourFacts() {
+    const num = 4;
+    let counter = 1;
+    while(counter < 5) {
+        counter++;
+        console.log(counter);
+        axios.get(`http://numbersapi.com/${num}?json`)
+        .then(data => {
+            $("#num-facts").append(`<li>${data.data.text}</li>`);
+        })
+        .catch(err => console.log(err));
+    }
+}
+
+$("#num-btn").on('click', getOneNumFacts);
+$("#num-4").on("click", getMoreFacts);
+$("#1-num-4-fax").on('click', oneNumFourFacts);
